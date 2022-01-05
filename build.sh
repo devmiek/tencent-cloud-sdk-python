4b02c49c587da002c96fff6cadbd0cf6dbae40f9
Handle <king@xiaoyy.org>
2022-01-05 22:58:46 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install build
+rm -rf ./dist
+
+build_package() {
+    python3 -m build ./packages/$1 -o ./dist
+}
+
+build_package tencent-cloud-sdk-auth
+build_package tencent-cloud-sdk-common
+build_package tencent-cloud-sdk-core
+build_package tencent-cloud-sdk-serverless-database
+build_package tencent-cloud-sdk-serverless-functions
+build_package ..
